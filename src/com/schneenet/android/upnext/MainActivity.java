d320663b10cf37b31a55a5ed3cb78d1b6239b80d
Matt Schneeberger <helluvamatt@gmail.com>
2013-03-25 04:00:49 -0500

    Bare project with some extra code
    Bare Eclipse Android project with some extra code to facilitate what we
    want to do. We also depend on some libraries that exist as other
    repositories in this organization.

---
+++ src/com/schneenet/android/upnext/MainActivity.java
@@ -0,0 +1,16 @@
+package com.schneenet.android.upnext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+	
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+	}
+	
+}
